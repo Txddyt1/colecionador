0df29f497563a363f943e4be0ae5f4bef89b209b
Txddyt1 <igorschuller40@gmail.com>
2024-11-14 14:49:18 -0300

    Add files via upload

---
+++ back-end/colecionador/src/main/java/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import java.io.InputStream;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import model.bo.UsuarioBO;
+import model.vo.UsuarioVO;
+
+@Path("/usuario")
+public class UsuarioController {
+    
+    @POST
+    @Path("/cadastrar")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public UsuarioVO cadastrarUsuarioController(@FormDataParam("usuarioVO") InputStream usuarioInputStream) throws Exception {
+        UsuarioBO usuarioBO = new UsuarioBO();
+        return usuarioBO.cadastrarUsuarioBO(usuarioInputStream);
+    }
+
+    @PUT
+    @Path("/atualizar")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Boolean atualizarUsuarioController(@FormDataParam("usuarioVO") InputStream usuarioInputStream) throws Exception {
+        UsuarioBO usuarioBO = new UsuarioBO();
+        return usuarioBO.atualizarUsuarioBO(usuarioInputStream);
+    }
+
+    @DELETE
+    @Path("/excluir")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Boolean excluirUsuarioController (UsuarioVO usuarioVO) {
+        UsuarioBO usuarioBO = new UsuarioBO();
+        return usuarioBO.excluirUsuarioBO(usuarioVO);
+    }
+}
